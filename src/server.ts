8cb6fb568015edbba3cb55989552afeb8e63441a
Josh Nussbaum <joshnuss@gmail.com>
2025-02-11 22:18:40 +0800

    Adds basic counter

--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,34 @@
-import type * as Party from "partykit/server";
+import type { Server, Room, Connection } from "partykit/server"
 
-export default class Server implements Party.Server {
-  constructor(readonly room: Party.Room) {}
+export default class CounterServer implements Server {
+  constructor(readonly room: Room) {}
 
-  onConnect(conn: Party.Connection, ctx: Party.ConnectionContext) {
-    // A websocket just connected!
-    console.log(
-      `Connected:
-  id: ${conn.id}
-  room: ${this.room.id}
-  url: ${new URL(ctx.request.url).pathname}`
-    );
+  value = 0
 
-    // let's send a message to the connection
-    conn.send("hello from server");
+  onConnect(conn: Connection) {
+    conn.send(this.value)
   }
 
-  onMessage(message: string, sender: Party.Connection) {
-    // let's log the message
-    console.log(`connection ${sender.id} sent message: ${message}`);
-    // as well as broadcast it to all the other connections in the room...
-    this.room.broadcast(
-      `${sender.id}: ${message}`,
-      // ...except for the connection it came from
-      [sender.id]
-    );
+  onMessage(message: string) {
+    switch (message) {
+      case 'reset':
+        this.value = 0
+        break
+
+      case "increment":
+      case "inc":
+        this.value++
+        break
+
+      case "decrement":
+      case "dec":
+        this.value--
+        break
+
+      default:
+        this.value = +message
+    }
+
+    this.room.broadcast(this.value)
   }
 }
-
-Server satisfies Party.Worker;
