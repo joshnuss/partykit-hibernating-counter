9ca5fbbf79ce2155ebda1f0d6c817f135a97d08c
Josh Nussbaum <joshnuss@gmail.com>
2025-02-11 23:35:20 +0800

    Waits for content to be loaded

--- src/client.ts
+++ src/client.ts
@@ -3,39 +3,42 @@ import PartySocket from "partysocket"
 
 declare const PARTYKIT_HOST: string
 
-// connect to counter server
-const conn = new PartySocket({
-  host: PARTYKIT_HOST,
-  room: "my-new-room",
-})
+document.addEventListener("DOMContentLoaded", () => {
 
-// get DOM elements
-const value = document.querySelector('#value')
-const increment = document.querySelector('#increment')
-const decrement = document.querySelector('#decrement')
+  // connect to counter server
+  const conn = new PartySocket({
+    host: PARTYKIT_HOST,
+    room: "my-new-room",
+  })
 
-// handle increment button click
-increment.addEventListener('click', () => {
-  // send `increment` message
-  conn.send('increment')
-})
+  // get DOM elements
+  const value = document.querySelector('#value')
+  const increment = document.querySelector('#increment')
+  const decrement = document.querySelector('#decrement')
 
-// handle decrement button click
-decrement.addEventListener('click', () => {
-  // send `decrement` message
-  conn.send('decrement')
-})
+  // handle increment button click
+  increment.addEventListener('click', () => {
+    // send `increment` message
+    conn.send('increment')
+  })
 
-// handle new value from server
-conn.addEventListener("message", (event) => {
-  // remove highlighting
-  value.classList.remove('highlight')
+  // handle decrement button click
+  decrement.addEventListener('click', () => {
+    // send `decrement` message
+    conn.send('decrement')
+  })
 
-  // update DOM
-  value.innerText = event.data
+  // handle new value from server
+  conn.addEventListener("message", (event) => {
+    // remove highlighting
+    value.classList.remove('highlight')
 
-  // add highlighting
-  setTimeout(() => {
-    value.classList.add('highlight')
-  }, 10)
+    // update DOM
+    value.innerText = event.data
+
+    // add highlighting
+    setTimeout(() => {
+      value.classList.add('highlight')
+    }, 10)
+  })
 })
